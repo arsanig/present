61dbd400997c5996a23515fe065ed134f8ece819
arsani <arsanig@users.noreply.github.com>
2020-07-31 21:59:41 -0400

    Fixes & new auth methods

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -5,7 +5,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db
+from src.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -39,7 +39,7 @@ def register():
     return render_template('auth/register.html')
 
 @bp.route('/login', methods=('GET', 'POST'))
-def login();
+def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -62,3 +62,29 @@ def login();
         flash(error)
 
     return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
